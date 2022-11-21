10ec18b034dc2e050821117a2a7d50c063405a70
hami-r <mynameissaitama54@gmail.com>
2022-11-21 21:56:29 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean isContinue = true;
+        while (isContinue) {
+            System.out.println("""
+                    <---------------MENU------------->
+                    1. Add Student
+                    2. Display All Student
+                    3. Search Student
+                    4. Delete Student
+                    5. Exit
+                    Select your option:\n""");
+
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+
+            }
+
+        }
+
+    }
+
+}
